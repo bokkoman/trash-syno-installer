bb41944ab2220b9e0fefaf624b9e5a303ab2e581
bokkoman <bokkoman@gmail.com>
2021-12-14 18:05:45 +0100

    Add files via upload

---
+++ menu_volume.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+mapfile -t volume_list_array < <(mount -l | grep -E "/volume[0-9]{1,2}\s" | awk '{ print $3 }' | sort -V) # printf '%s\n' "${volume_list_array[@]}"
+
+if [[ "${#volume_list_array[@]}" -eq '1' ]]; then
+    volume="${volume_list_array[0]}"
+else
+    createmenu() {
+        REPLY=""
+        volume=""
+        PS3=$'\n'"Please select a volume to use from the options: "
+        printf '\n'
+        select option; do # in "$@" is the default
+            if [[ "$REPLY" -gt "$#" ]]; then
+                printf '\n%s\n' "This is not a valid volume option, try again."
+            else
+                volume="$(printf '%s' "${option}")"
+                printf '\n'
+                read -erp "You selected $volume is this correct: " -i "yes" confirm
+                [[ "${confirm}" =~ ^[yY](es)?$ ]] && break
+            fi
+        done
+    }
+    createmenu "${volume_list_array[@]}"
+fi
