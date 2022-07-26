5b7a5087aef1c44c1bc6afdc0232ebc06300da08
bokkoman <bokkoman@gmail.com>
2022-07-26 13:50:01 +0200

    Update newtrash.sh

--- newtrash.sh
+++ newtrash.sh
@@ -498,7 +498,7 @@ get_app_compose() {
 
         [[ "${options,,}" =~ ^(sabnzbd)$ ]] && sed -r 's|- 8080:8080$|- 7080:8080|g' -i "${docker_conf_dir}/appdata/${1}.yml"
         [[ "${options,,}" =~ ^(dozzle)$ ]] && sed -r 's|- 8080:8080$|- 7081:8080|g' -i "${docker_conf_dir}/appdata/${1}.yml"
-        [[ "${options,,}" =~ ^(qbittorrent)$ ]] && sed -r 's|^VPN_ENABLED=true$|VPN_ENABLED=false|g' -i "${docker_conf_dir}/appdata/${1}.yml"
+        [[ "${options,,}" =~ ^(qbittorrent)$ ]] && sed -r 's|VPN_ENABLED=true|VPN_ENABLED=false|g' -i "${docker_conf_dir}/appdata/${1}.yml"
 
         sed -n 'p' "${docker_conf_dir}/appdata/${1}.yml" >> "${docker_conf_dir}/appdata/docker-compose.yml"
         rm -f "${docker_conf_dir}/appdata/${1}.yml"
